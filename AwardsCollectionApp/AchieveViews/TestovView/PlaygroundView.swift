5d318871e5d1359ea65d59a2eec341531d767827
Stanislav <te100ff@inbox.ru>
2021-06-18 17:00:15 +0300

    eyes problems

--- AwardsCollectionApp/AchieveViews/TestovView/PlaygroundView.swift
+++ AwardsCollectionApp/AchieveViews/TestovView/PlaygroundView.swift
@@ -12,13 +12,103 @@ struct PlaygroundView: View {
     let height: CGFloat
     
     var body: some View {
-        TestovLineOne(width: 300, height: 300)
-            .rotationEffect(.degrees(90))
-    }
-      
-}
+        GeometryReader { geometry in
+            let size = min(geometry.size.width, geometry.size.height)
+            let middle = size / 2
+            let nearLine = size * 0.1
+            let farLine = size * 0.9
+            
+            //            Path { path in
+            //                path.move(to: CGPoint(x: 0, y: middle))
+            //                path.addQuadCurve(
+            //                    to: CGPoint(x: width, y: middle),
+            //                    control: CGPoint(x: middle, y: -middle)
+            //                )
+            //            }
+            
+            Path { path in
+                path.addArc(
+                    center: CGPoint(x: middle, y: middle - nearLine),
+                    radius: middle/2,
+                    startAngle: .degrees(45),
+                    endAngle: .degrees(135),
+                    clockwise: true
+                )
+            }
+            
+            Path { path in
+                path.addRect(
+                    CGRect(
+                        x: middle - size/40,
+                        y: middle,
+                        width: size/20,
+                        height: middle/2
+                    )
+                )
+            }
+            
+            Path { path in
+                path.addRect(
+                    CGRect(
+                        x: middle - size/10,
+                        y: middle,
+                        width: size/20,
+                        height: middle/2.5
+                    )
+                )
+            }
+            
+            Path { path in
+                path.addRect(
+                    CGRect(
+                        x: middle + size/20,
+                        y: middle,
+                        width: size/20,
+                        height: middle/2.5
+                    )
+                )
+            }
+            
+            Path { path in
+                path.addRect(
+                    CGRect(
+                        x: middle + size/7.5,
+                        y: middle,
+                        width: size/20,
+                        height: middle/4
+                    )
+                )
+            }
+            
+            Path { path in
+                path.addRect(
+                    CGRect(
+                        x: middle - size/5.5,
+                        y: middle,
+                        width: size/20,
+                        height: middle/4
+                    )
+                )
+            }
+            
+           
+            
 
-        
+            Path { path in
+                path.move(to: CGPoint(x: middle - size/40, y: middle - size/40))
+                path.addCurve(
+                    to: CGPoint(x: middle - size/40, y: middle - size/40),
+                    control1: CGPoint(x: nearLine, y: farLine),
+                    control2: CGPoint(x: farLine, y: nearLine)
+                )
+            }
+            .stroke(Color.red, linewidth: 4)
+            
+        }
+        .frame(width: width, height: height)
+    }
+    
+}
 
 
 struct PlaygroundView_Previews: PreviewProvider {
--- AwardsCollectionApp/AchieveViews/TestovView/TestovLineTwo.swift
+++ AwardsCollectionApp/AchieveViews/TestovView/TestovLineTwo.swift
@@ -12,28 +12,8 @@ struct TestovLineTwo: View {
     let height: CGFloat
     
     var body: some View {
-        GeometryReader { geometry in
-            let size = min(geometry.size.width, geometry.size.height)
-            let middle = size / 2
-            let nearLine = size * 0.1
-            let farLine = size * 0.9
-            
-            Path { path in
-                path.move(to: CGPoint(x: 0, y: farLine))
-                path.addLine(to: CGPoint(x: nearLine, y: height))
-                path.addLine(to: CGPoint(x: width, y: nearLine))
-                path.addLine(to: CGPoint(x: farLine, y: 0))
-            }
-            .fill(
-                RadialGradient(
-                    gradient: Gradient(colors: [.white, .black]),
-                    center: .center,
-                    startRadius: geometry.size.width * 0.01,
-                    endRadius: geometry.size.width * 0.4
-                )
-            )
-        }
-        .frame(width: width, height: height)
+        TestovLineOne(width: 300, height: 300)
+            .rotationEffect(.degrees(90))
     }
 }
 
